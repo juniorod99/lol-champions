3c304e6d8d9b620d0fa413274cbb139138c6266d
Raimundo <juniorod99@gmail.com>
2023-11-14 21:28:25 -0300

    Adicionado slide de skins.

--- assets/js/champion-detalhes.js
+++ assets/js/champion-detalhes.js
@@ -12,7 +12,7 @@ const getChampionData = async (id) => {
   const response = await fetch(url);
   const data = await response.json();
   const championData = data.data[id];
-  console.log(championData);
+  // console.log(championData);
   createChampionPage(championData);
 };
 
@@ -30,21 +30,76 @@ const createChampionPage = (champion) => {
   console.log(spells);
   const skins = champion.skins;
   console.log(skins);
-  const championBanner = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`;
-  console.log(championBanner);
+  // const championBanner = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`;
+  // console.log(championBanner);
+  const listaDeSkins = gerarListaSkins(skins);
+  console.log(listaDeSkins);
+  const listaSpells = gerarHabilidades(passive, spells);
+  console.log(listaSpells);
 };
 
+function gerarHabilidades(passive, spells) {
+  const spellsList = document.createElement("div");
+  spellsList.classList.add("spells-text");
+  console.log(passive.name);
+  console.log(passive.description);
+  console.log(spells[0].name);
+  console.log(spells[0].description);
+  const spellsInnerHTML = `
+  <div class="spells-images">
+    <img src="https://ddragon.leagueoflegends.com/cdn/13.22.1/img/passive/${passive.image.full}" alt="" class="item" onclick="mostrarDiv('spell-1')">
+    <img src="https://ddragon.leagueoflegends.com/cdn/13.22.1/img/spell/${spells[0].image.full}" alt="" class="item" onclick="mostrarDiv('spell-2')">
+    <img src="https://ddragon.leagueoflegends.com/cdn/13.22.1/img/spell/${spells[1].image.full}" alt="" class="item" onclick="mostrarDiv('spell-3')">
+    <img src="https://ddragon.leagueoflegends.com/cdn/13.22.1/img/spell/${spells[2].image.full}" alt="" class="item" onclick="mostrarDiv('spell-4')">
+    <img src="https://ddragon.leagueoflegends.com/cdn/13.22.1/img/spell/${spells[3].image.full}" alt="" class="item" onclick="mostrarDiv('spell-5')">
+  </div>
+  <div class="spell-details ativo" id="spell-1">
+    <h3>P - ${passive.name}</h3>
+    <p>${passive.description}</p>
+  </div>
+  <div class="spell-details" id="spell-2">
+    <h3>Q - ${spells[0].name}</h3>
+    <p>${spells[0].description}</p>
+  </div>
+  <div class="spell-details" id="spell-3">
+    <h3>W - ${spells[1].name}</h3>
+    <p>${spells[1].description}</p>
+  </div>
+  <div class="spell-details" id="spell-4">
+    <h3>E - ${spells[2].name}</h3>
+    <p>${spells[2].description}</p>
+  </div>
+  <div class="spell-details" id="spell-5">
+    <h3>R - ${spells[3].name}</h3>
+    <p>${spells[3].description}</p>
+  </div>`;
+
+  spellsList.innerHTML = spellsInnerHTML;
+  return spellsList;
+}
+
+function gerarListaSkins(skins) {
+  const lista = document.createElement("ul");
+  lista.classList.add("splide__list");
+  let listaInnerHTML = "";
+
+  for (skin in skins) {
+    listaInnerHTML += `
+    <li class="splide__slide">
+      <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_${skins[skin].num}.jpg">
+    </li>`;
+  }
+
+  lista.innerHTML = listaInnerHTML;
+  return lista;
+}
+
 function mostrarDiv(idDivParaMostrar) {
-  // Obtém todas as divs com a classe 'minha-div'
   var todasDivs = document.querySelectorAll(".spell-details");
-  console.log(todasDivs);
-  // Itera sobre todas as divs e mostra/oculta conforme necessário
   todasDivs.forEach(function (div) {
     if (div.id === idDivParaMostrar) {
-      // Se a div for a que queremos mostrar, exibe
       div.style.display = "block";
     } else {
-      // Se não for a div que queremos mostrar, oculta
       div.style.display = "none";
     }
   });
